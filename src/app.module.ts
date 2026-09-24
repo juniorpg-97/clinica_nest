9b0052d47d721380532d00740091a535fbf8add7
unknown <portohector97@gmail.com>
2026-09-24 18:57:59 -0500

    feat: ConfigService con validacion de variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
 import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { PacientesModule } from './pacientes/pacientes.module.js';
 import { MedicosModule } from './medicos/medicos.module.js';
@@ -13,6 +14,11 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+        JWT_SECRET: Joi.string().min(10).required(),
+        PORT: Joi.number().default(3000),
+      }),
     }),
     PrismaModule,
     PacientesModule,
